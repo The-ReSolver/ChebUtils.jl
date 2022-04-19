6b3dc8c9be1d1b22e2c413931459ac8ee8ca391f
Thomas Burton <tburton5572@gmail.com>
2022-04-19 12:18:36 +0100

    test specific dependencies (again...)

--- src/ChebUtils.jl
+++ src/ChebUtils.jl
@@ -1,6 +1,6 @@
 module ChebUtils
 
-import LinearAlgebra
+using LinearAlgebra
 
 export chebpts, chebdiff, chebddiff, chebws
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,7 @@ using Test
     @test size(points) == (rint,)
     @test points[1] == 1.0
     @test points[end] == -1.0
-    @test points[rand_ind] == Float64(cos(π*(rand_ind - 1)/(rint - 1)))
+    @test points[rand_ind] ≈ Float64(cos(π*(rand_ind - 1)/(rint - 1)))
 end
 
 @testset "Differentiation matrices  " begin
